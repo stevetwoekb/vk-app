8892963f47216ca89a30226780368234d67d4ed4
snifontov <s.nifontov@artsofte.ru>
2020-04-23 23:07:57 +0500

    1
    d

---
+++ precache-manifest.b4d42480254929aa1632d75f43592de6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd19c3a05c5815771083f9e5c72934e",
+    "url": "/vk-app/index.html"
+  },
+  {
+    "revision": "adaedd8440134b1cb473",
+    "url": "/vk-app/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "d14f53c4a271e05d687e",
+    "url": "/vk-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "adaedd8440134b1cb473",
+    "url": "/vk-app/static/js/2.22d1853d.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/vk-app/static/js/2.22d1853d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14f53c4a271e05d687e",
+    "url": "/vk-app/static/js/main.4350ef76.chunk.js"
+  },
+  {
+    "revision": "08e7acf106a1214c4425",
+    "url": "/vk-app/static/js/runtime-main.cb9f44b0.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-app/static/media/persik.4e1ec840.png"
+  }
+]);
